29b01b683c7016c69602dd2ad342b8bbdac984c5
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2023-08-08 23:45:13 +0530

    log in user

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 
 // register new user
@@ -7,8 +8,8 @@ router.post('/register', async (req, res) => {
     try {
         // check if user already exists
         const userExists = await User.findOne({ email: req.body.email })
-        if(userExists) {
-            return res.send({success: false, message: 'User already exists'})
+        if (userExists) {
+            return res.send({ success: false, message: 'User already exists' })
         }
 
         // hash password
@@ -19,7 +20,34 @@ router.post('/register', async (req, res) => {
         // create new user
         const newUser = new User(req.body)
         const savedUser = await newUser.save()
-        return res.send({success: true, message: 'User created successfully', data: savedUser})
+        return res.send({ success: true, message: 'User created successfully', data: savedUser })
+    } catch (error) {
+        return res.send({ success: false, message: error.message })
+    }
+})
+
+// login user
+router.post('/login', async (req, res) => {
+    try {
+        // check if user exists
+        const user = await User.findOne({ email: req.body.email })
+        if (!user) {
+            return res.send({ success: false, message: 'User does not exist' })
+        }
+        // check if password is correct
+        const validPassword = await bcrypt.compare(req.body.password, user.password)
+        if (!validPassword) {
+            return res.send({ success: false, message: 'Invalid password' })
+        }
+
+        // create token
+        const token = jwt.sign(
+            {userid: user._id,}, 
+            process.env.JWT_SECRET, 
+            { expiresIn: '1d' }
+        )
+
+        return res.send({ success: true, message: 'Login successful', data: token })
     } catch (error) {
         return res.send({ success: false, message: error.message })
     }
