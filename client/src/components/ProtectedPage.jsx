a39140dfdb8c205458a53ff24f643eedea3dd046
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2023-08-10 18:45:38 +0530

    header section

--- client/src/components/ProtectedPage.jsx
+++ client/src/components/ProtectedPage.jsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react'
-import { message } from 'antd'
+import { Tooltip, message } from 'antd'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { useSelector } from 'react-redux'
@@ -13,7 +13,7 @@ const ProtectedPage = ({ children }) => {
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
-    const {currentUser} = useSelector(state => state.users) 
+    const { currentUser } = useSelector(state => state.users)
 
     const getCurrentUser = useCallback(async () => {
         try {
@@ -21,7 +21,7 @@ const ProtectedPage = ({ children }) => {
             const response = await GetCurrentUser()
             dispatch(SetLoading(false))
             console.log(response);
-            if(response.success) {
+            if (response.success) {
                 message.success(response.message)
                 dispatch(SetCurrentUser(response.data))
             } else {
@@ -32,10 +32,10 @@ const ProtectedPage = ({ children }) => {
             message.error(error.message)
         }
     }, [dispatch])
-    
+
 
     useEffect(() => {
-        if(localStorage.getItem('token') !== null) {
+        if (localStorage.getItem('token') !== null) {
             getCurrentUser()
         } else {
             navigate('/login')
@@ -44,9 +44,34 @@ const ProtectedPage = ({ children }) => {
 
     return (
         currentUser ? <div>
-            <h1>{getLoggedInUsername(currentUser)}</h1>
-            {children}
-        </div> :  <></>
+            {/* header */}
+            <div className='flex justify-between items-center bg-primary text-white px-5 py-3'>
+                <div className="flex flex-col uppercase">
+                    <h1 className="text-2xl uppercase">
+                        Blood Bank
+                    </h1>
+                    <span className='text-xs'>
+                        {currentUser?.userType.toUpperCase()}
+                    </span>
+                </div>
+                <div className='flex items-center'>
+                    <i className='ri-shield-user-fill'></i>
+                    <span className='mr-5 ml-1 text-xl cursor-pointer'>{getLoggedInUsername(currentUser)}</span>
+
+                    <Tooltip title='logout' className='ml-5 cursor-pointer'>
+                        <i className='ri-logout-circle-r-line' onClick={() => {
+                            localStorage.removeItem('token')
+                            navigate('/login')
+                        }}></i>
+                    </Tooltip>
+                </div>
+            </div>
+
+            {/* content */}
+            <div className="p-5">
+                {children}
+            </div>
+        </div> : <></>
     )
 }
 
