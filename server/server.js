d5fd1911d7aec5211bb33c1469b4b9db48a02ec0
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2023-08-13 13:46:36 +0530

    deployment config

--- server/server.js
+++ server/server.js
@@ -17,6 +17,17 @@ app.use('/api/dashboard', dashboardRoute)
 
 const port = process.env.PORT || 5000
 
+// deployment config
+const path = require("path");
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => {
     console.log('Server is up and running in', port);
 })
